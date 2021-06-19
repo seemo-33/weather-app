e079c3f957dd5c37526ee3fd085ab12478cc712e
seemo-33 <annasimmi03@gmail.com>
2021-06-20 00:53:27 +0530

    setting up application for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,13 @@ const dataElement = document.querySelector('input')
 const msg1 = document.querySelector('#message-1')
 const msg2 = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
     const location = dataElement.value
     msg1.textContent = 'Searching...'
     msg2.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + location).then((res) => {
+    fetch('/weather?address=' + location).then((res) => {
     res.json().then((dataWeather) => {
         if(dataWeather.error) {
             msg1.textContent = dataWeather.error
