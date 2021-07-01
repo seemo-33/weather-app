f63f93d1a01ef0035e409d39ca339475f75c4720
seemo-33 <annasimmi03@gmail.com>
2021-07-01 21:15:36 +0530

    API Keys changed

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=your access key'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2VlbW8tMTMiLCJhIjoiY2twd3JweWp2MDNyYTJybG95d2l4MzhweCJ9.y0UGqEkmZnEvsC6eEeHD5w'
     request({url, json: true}, (error, { body }) => {
         if (error) {
             callback('Unable to access Geo Location Services!', undefined)
